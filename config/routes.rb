52a34a915bdf04d9e622b606d3bb773f5c2d7dd0
Amir Barylko <amir@barylko.com>
2011-11-20 22:13:53 -0600

    Added route for root to point to movies#index, removed default index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Movielib::Application.routes.draw do
   resources :movies
 
+  root to: 'movies#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
