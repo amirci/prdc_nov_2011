7aaaf2c50f597d5452b5e0e2d4ea46e248521976
Amir Barylko <amir@barylko.com>
2011-11-20 22:38:56 -0600

    Added create method to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,10 @@
 class ReviewsController < ApplicationController
+  
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.reviews.create(params[:review])
+    @reviews = Review.all
+    redirect_to @movie
+  end
+  
 end
