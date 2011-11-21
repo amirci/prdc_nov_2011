4f212c8df6f1ec3433f8109b4dafa302a2afc03d
Amir Barylko <amir@barylko.com>
2011-11-20 21:42:01 -0600

    First commit after generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movielib::Application.config.secret_token = 'f329ec18081ccaa99569d9e6c4dd2f5c1551ca0cacb34efa6f4360c22f7548e814d1ffbbbe5d0f71ab69142f93508dd207c57a7238b4122f92cc9d4687eb28fd'
